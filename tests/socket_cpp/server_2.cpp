e2ec5871cf73ad4be02bed8a0bdc9888c431a8ad
RAV263 <nickiforov.nik@gmail.com>
2019-03-23 12:07:30 +0300

    Adding client cosket class, new client and server example

--- tests/socket_cpp/server_2.cpp
+++ tests/socket_cpp/server_2.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
 #include <string>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
 #include "sockets.h"
-
+#include "logs.h"
 
 enum {
     PORT_NUM = 2917,
@@ -17,23 +15,21 @@ int main() {
 
     Net::ServerSocket server(PORT_NUM);
 
-    bind(server.get_socket(),(const sockaddr *) server.get_sock_addr(), server.get_sock_addr_size());
-
-    std::cout << "Waiting for client... " << std::endl;
+    Logs::logln("Waiting for client... ");
 
     socklen_t size = server.get_sock_addr_size();
     listen(server.get_socket(), 1);
     
-    std::cout << "Connecting to client... " << std::endl;
+    Logs::log("Connecting to client... ");
 
     int client = accept(server.get_socket(), (struct sockaddr*) server.get_sock_addr(), &size);
 
     if (client < 0) {
-        std::cout << "Faled" << std::endl;
+        Logs::logln_err("Faled");
         return 1;
     }
 
-    std::cout << "Connected" << std::endl;
+    Logs::logln("Successfull");
 
     close(client);
 }
--- tests/socket_cpp/sockets.h
+++ tests/socket_cpp/sockets.h
@@ -1,6 +1,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include <string>
 #include <iostream>
 
@@ -17,28 +19,90 @@ namespace Net {
     
     public:
         ServerSocket(int port) {
-            Logs::log("Starting server.. ");
-            sock = socket(AF_INET, SOCK_STREAM, 0);
+            Logs::log("Creating socket.. ");
+            this->sock = socket(AF_INET, SOCK_STREAM, 0);
             
-            sock_addr.sin_family = AF_INET;
-            sock_addr.sin_addr.s_addr = htons(INADDR_ANY);
-            sock_addr.sin_port = htons(port);
+            if (sock < 0) {
+                Logs::logln("Falied");
+                throw 1;
+            }
+
+            Logs::logln("Successfull");
+
+
+            this->sock_addr.sin_family = AF_INET;
+            this->sock_addr.sin_addr.s_addr = htons(INADDR_ANY);
+            this->sock_addr.sin_port = htons(port);
+            
+            Logs::log("Starting server.. ");
+            if (bind(this->sock, (const sockaddr *) &(this->sock_addr), sizeof(this->sock_addr)) < 0) {
+                Logs::logln("Falied");
+                throw 1;
+            }
+            Logs::logln("Successfull");
         }
 
         int get_socket() const {
-            return sock;
+            return this->sock;
         }
 
         const struct sockaddr_in *get_sock_addr() const {
-            return &sock_addr;
+            return &(this->sock_addr);
         }
 
-        size_t get_sock_addr_size() {
-            return sizeof(sock_addr);
+        size_t get_sock_addr_size() const {
+            return sizeof(this->sock_addr);
         }
 
         ~ServerSocket() {
-            close(sock);
+            close(this->sock);
+        }
+    };
+
+    class ClientSocket {
+    private:
+        int sock;
+        struct sockaddr_in sock_addr;
+    public:
+        ClientSocket(int port, const std::string &ip) {
+            Logs::log("Creating socket.. ");
+            this->sock = socket(AF_INET, SOCK_STREAM, 0);
+
+            if (sock < 0) {
+                Logs::logln("Falied");
+                throw 1;
+            }
+            Logs::logln("Successfull");
+            
+            this->sock_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+            this->sock_addr.sin_family = AF_INET;
+            this->sock_addr.sin_port = htons(port);
+        }
+
+        void connect_to_server() {
+            Logs::log("Connecting.. ");
+            if (connect(this->sock, (const sockaddr *) &(this->sock_addr), sizeof(this->sock_addr)) < 0) {
+                Logs::logln("Falied");
+                throw 1;
+            }
+
+            Logs::logln("Successfull");
+        }
+
+        int get_socket() const {
+            return this->sock;
+        }
+
+        const struct sockaddr_in *get_sock_addr() const {
+            return &(this->sock_addr);
+        }
+
+        size_t get_sock_addr_size() const {
+            return sizeof(this->sock_addr);
+        }
+
+        ~ClientSocket() {
+            close(this->sock);
         }
     };
 }
