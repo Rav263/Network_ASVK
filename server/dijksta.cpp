156cfff6f092e9a4b97162399a254c1238b2f24c
RAV263 <nickiforov.nik@gmail.com>
2019-03-30 12:47:57 +0300

    improving some bugs in dijkstra algorithm

--- server/dijksta.cpp
+++ server/dijksta.cpp
@@ -10,9 +10,8 @@
 #include "../headers/logs.h"
 
 
-void calc_vertex(Vertex vertex, Graph &graph, std::set<Vertex> &queue, VertexList &vertexes, std::set<Vertex> used) {
+void calc_vertex(Vertex vertex, Graph &graph, VertexList &vertexes, std::set<Vertex> used) {
     for (auto edge : graph[vertex]) {
-        if (used.count(edge.first) > 0) continue;
         if (vertexes.count(edge.first) > 0 && vertexes[edge.first] > vertexes[vertex] + edge.second) {
             vertexes[edge.first] = vertexes[vertex] + edge.second;
         }
@@ -20,29 +19,43 @@ void calc_vertex(Vertex vertex, Graph &graph, std::set<Vertex> &queue, VertexLis
         if (vertexes.count(edge.first) == 0) {
             vertexes.insert(Edge(edge.first, vertexes[vertex] + edge.second));
         }
-        
-        queue.insert(edge.first);
     }
 }
 
 
+void add_vertexes(Vertex now_vertex, VertexList &list, 
+        std::set<Vertex> &used, std::set<std::pair<Mass, Vertex>> &queue, 
+        Graph &graph, std::set<Vertex> &is_in_queue) {
+    for (Edge now_edge : graph[now_vertex]) {
+        if (not used.count(now_edge.first) && not is_in_queue.count(now_edge.first)) {
+            Mass ver_mass = (not list.count(now_edge.first)) * INT64_MAX;
+            ver_mass = ver_mass > 0 ? ver_mass : list[now_edge.first];
+            queue.insert(std::pair<Mass, Vertex>(ver_mass, now_edge.first));
+            is_in_queue.insert(now_edge.first);
+        }
+    }
+}
+
 
 void calc_path(Path &path, Graph &graph, Vertex start, Vertex end) {
-    std::set<Vertex> ver_queue;
-    ver_queue.insert(start); 
+    std::set<std::pair<Mass, Vertex>> ver_queue;
+    std::set<Vertex> is_in_queue;
+    ver_queue.insert(std::pair<Mass, Vertex>(0, start)); 
     
     VertexList vertexes;
     vertexes[start] = 0;
 
     std::set<Vertex> used;
 
-    for (auto now_vertex : ver_queue) {
-        std::cout << now_vertex << std::endl;
+    for (auto now_pair : ver_queue) {
+        Vertex now_vertex = now_pair.second;
+        
         used.insert(now_vertex);
-        calc_vertex(now_vertex, graph, ver_queue, vertexes, used);
-        ver_queue.erase(now_vertex);
-
-        for (auto shit : vertexes) std::cout << "(" << shit.first << ", " << shit.second << ")" << std::endl;
+        std::cout << now_vertex << std::endl;
+        calc_vertex(now_vertex, graph, vertexes, used);
+        
+        add_vertexes(now_vertex, vertexes,  used, ver_queue, graph, is_in_queue);
+        ver_queue.erase(now_pair);
     }
     
     path.push_back(Edge(end, vertexes[end]));
